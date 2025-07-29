e89be401100c4131b5866ad95af2c1fa5f37776d
hacker098761 <hackerrank2025@outlook.com>
2025-07-30 00:18:35 +0530

    Fix global is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
